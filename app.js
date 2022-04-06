bc93fa748b5a5f9f268236dd5c0c44a55ba2f1c6
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2022-04-06 18:09:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*const http = require('http');
+const http = require('http');
 
 const hostname = '0.0.0.0';
 const port = 3000;
@@ -12,8 +12,8 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-*/
 
+/*
 const express = require('express');
 
 const app = express();
@@ -30,4 +30,4 @@ router.get('/', function(req, res){
 
 app.use('/', router);
 
-app.liste(process.env.port || 3000);
+app.liste(process.env.port || 3000);*/
