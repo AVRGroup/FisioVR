b08bc98056381ef19d20fdc28dbef368f235b5e3
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2022-04-06 09:36:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, '127.0.0.1', () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
