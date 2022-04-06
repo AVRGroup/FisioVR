fd3ac0ba5b1dd0b12fd2584fada120218d95dc6f
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2022-04-06 09:14:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 const hostname = '200.131.17.17';
 const port = 10800;
@@ -12,3 +12,21 @@ const server = http.createServer((req, res) => {
 server.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
+
+let http = require('http')
+let port = 10800
+let ip = 'localhost'
+
+let server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    res.end('<h1>Home</h1>')
+  }
+
+  res.end('<h1>URL sem resposta definida!</h1>')
+})
+
+server.listen(port, ip, () => {
+  console.log(`Servidor rodando em http://${ip}:${port}`)
+  console.log('Para derrubar o servidor: ctrl + c');
+})
