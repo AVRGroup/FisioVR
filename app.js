31c8d810fb6fdae8d31c95f21c1caeb9c0f209cf
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2022-04-06 09:40:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '200.131.17.17';
-const port = 10800;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, '127.0.0.1', () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
