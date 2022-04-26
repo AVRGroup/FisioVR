17e2c097398c84afb0f8bf605a252fa08f42caca
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2022-04-26 15:12:49 -0300

    ajuste funcao async

--- indexx.js
+++ indexx.js
@@ -1,8 +1,8 @@
 console.log("ssssss");
-async () => {
+(async () => {
     const db = require("./test");
     console.log("Conectou!");
     console.log('SELECT * FROM USUARIO');
     const clients = await db.selectCustomers();
     console.log(clients);
-};
+}) ();
