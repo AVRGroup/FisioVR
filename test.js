50e787632c088936301af01128468a8fd943a3c5
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2022-04-26 14:17:52 -0300

    teste

---
+++ test.js
@@ -0,0 +1,20 @@
+
+
+async function connect(){
+  if(global.connection && global.connection.state !== 'disconnected')
+    return global.connection;
+
+  
+  var mysql = require('mysql2/promise');
+  var connection = await mysql.createConnection("mysql://root:fisiovr@localhost:3000/BD_teste");
+  console.log("deu certo amem amem");
+  return connection;
+}
+connect();
+/*
+async function selectCustomers() {
+    const conn = await connect();
+    return await = conn.query('SELECT * FROM profissional');
+}
+*/
+module.exports = {}
