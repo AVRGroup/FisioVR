33ccb5555642e6930c2fb4fdfc35f687e1e5fe58
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-05-09 19:43:03 -0300

    Update ConexaoBD.js

--- ConexaoBD.js
+++ ConexaoBD.js
@@ -15,7 +15,7 @@ console.log("ok");
 
 pool.getConnection(function(err, connection) {
   // Use the connection
-  pool.query( 'SELECT * FROM usu',[login], function(err, rows) {
+  pool.query( 'SELECT * FROM usu', function(err, rows) {
     // And done with the connection.
     connection.release();
 
