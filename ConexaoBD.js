bdca505d05f5c73aa7c59877f58b8526c44c4030
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-05-08 23:39:20 -0300

    Update ConexaoBD.js

--- ConexaoBD.js
+++ ConexaoBD.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
 	host: '200.131.17.17',
 	port: 10800,
 	user: 'root',
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
 	database:'BD_teste'
 });
 
-connection.connect(function(err){
+pool.connect(function(err){
  if(err) return console.log(err); 
  console.log('conectou!');
  });
