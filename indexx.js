3465d8ca115f36dbd62f86ecc859519a7f012fdc
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-05-10 00:20:03 -0300

    Update indexx.js

--- indexx.js
+++ indexx.js
@@ -1,8 +1,14 @@
+async function selectUsu(){
+    const conn = await connect();
+    const [rows] = await conn.query('SELECT * FROM usu');
+    return rows;
+}
+
 (async () => {
     const tt = require("./test");
     console.log('Começou!');
  
     console.log('SELECT * FROM usu');
-    const usuarios = await tt.selectUsu();
+    const usuarios = await selectUsu();
     console.log(usuarios);
 })();
