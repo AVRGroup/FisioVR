f40f24d67353e307714e2ffd0adb8f688800c50b
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-05-10 00:23:59 -0300

    Update test.js

--- test.js
+++ test.js
@@ -15,15 +15,15 @@ async function connect(){
 
 connect();
 
-/*
+
 async function selectUsu(){
     const conn = await connect();
-    const [rows] = await conn.query('SELECT * FROM usu');
-    return rows;
+    //const [rows] = await conn.query('SELECT * FROM usu');
+    return await conn.query('SELECT * FROM usu');
 }
 
 module.exports = {selectUsu}
-*/
+
 
 /*
 async function selectCustomers() {
