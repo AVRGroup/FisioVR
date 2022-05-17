7edeedf545888f0cb586919b369c70ef1aff7cdb
André Luiz Cunha de Oliveira <54187669+andrelcol@users.noreply.github.com>
2022-05-17 09:37:24 -0300

    verificacao de senha

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,39 @@
+const mysql = require("mysql2");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form submited");
+
+    const { user, password, passwordConfirm } = req.body;
+
+    db.query('SELECT login FROM usu WHERE login = ?', [user], async (error, results) => {
+        if(error) {
+            console.log(error);
+        }
+
+        if(results.length > 0) {
+            return res.render('cadastro', {
+                message: 'Usuario pronto'
+            })
+        } else if(password !== passwordConfirm) {
+            return res.render('cadastro', {
+                message: 'Senhas diferentes'
+            })
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+    });
+
+
+
 }
