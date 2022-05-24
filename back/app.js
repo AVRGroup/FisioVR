569e09cec1d47f418dd2dc1c3916c4e7964f4ba4
André Luiz Cunha de Oliveira <andreoliveira@ice.ufjf.br>
2022-05-24 10:26:59 -0300

    dotenv fixed

--- back/app.js
+++ back/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require('path');
 const mysql = require("mysql2");
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 const cookieParser = require("cookie-parser");
 
 
