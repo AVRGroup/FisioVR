cb8d58008729087b0df51ca54fc7d67d35708a7d
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2022-05-25 13:06:18 -0300

    elevacao lateral nova sintaxe

--- front/src/js/exercises/LateralRaise.js
+++ front/src/js/exercises/LateralRaise.js
@@ -3,32 +3,22 @@ import { init, angles } from '../poseNet';
 
 const deg5 = Math.PI / 36;
 const deg90 = Math.PI / 2;
-let rightShoulder = 0, leftShoulder = 0;
-const lateralRaise = new Exercise({ name: 'Elevação Lateral', sets: 1, leftReps: 2, rightReps: 2, rest: 3 });
 
-let vleft = false, vright = false;
-
-lateralRaise.verify = (keipoints) => {
-    rightShoulder = angles[4];
-    leftShoulder = angles[5];
-
-    vleft = (leftShoulder > (deg90 - deg5) && leftShoulder < (deg90 + deg5));
-    vright = (rightShoulder > (deg90 - deg5) && rightShoulder < (deg90 + deg5))
-
-    return {
-        left: vleft,
-        right: vright
-    }
-}
-
-lateralRaise.reset = () => {
-    rightShoulder = angles[4];
-    leftShoulder = angles[5];
-
-    return {
-        left: (leftShoulder < deg90 - deg5 - deg5),
-        right: (rightShoulder < deg90 - deg5 - deg5)
-    }
-}
+const lateralRaise = new Exercise({
+    name: 'Elevação Lateral',
+    sets: 1,
+    leftReps: 2,
+    rightReps: 2,
+    rest: 3,
+    concentric: {
+        leftShoulder: deg90,
+        rightShoulder: deg90
+    },
+    eccentric: {
+        leftShoulder: deg90 * 0.25,
+        rightShoulder: deg90 * 0.25
+    },
+    margin: deg5
+}, angles);
 
 init(lateralRaise);
