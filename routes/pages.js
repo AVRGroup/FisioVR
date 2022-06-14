8ee6810321b61d5fcb3bb57d200f92aef825ff72
André Luiz Cunha de Oliveira <andreoliveira@ice.ufjf.br>
2022-06-14 10:58:24 -0300

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -14,7 +14,8 @@ router.get('/cadastro', (req, res) => {
 });
 
 router.get('/paciente', authController.isLoggedIn, (req, res) => {
-    if( req.usuario && req.usuario.tipo_usuario == 3 ) {
+    console.log(usuario.id_tipo_usuario)
+    if( req.usuario && req.usuario.id_tipo_usuario == 3 ) {
         res.render('paciente', {
             user: req.usuario
 
