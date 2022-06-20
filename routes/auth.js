69d8c647cd73ea1522ddce6c7b3b6aaf7f5affa6
André Luiz Cunha de Oliveira <andreoliveira@ice.ufjf.br>
2022-06-20 16:24:54 -0300

    log

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const authController = require("../controllers/auth");
 const router = express.Router();
 
 router.post('/cadastro', authController.register);
-
+console.log(user.id_paciente + "authjs");
 router.post('/login', authController.login);
 
 router.get('/logout', authController.logout);
