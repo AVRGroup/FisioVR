43745fb1d25cbff00796b1ec922298e2e7160509
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-06-27 22:14:17 -0300

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -110,10 +110,10 @@ router.get('/profile', authController.isLoggedIn, (req, res) => {
     
 });
 
-router.get('/profissional_profile', consulta.consultapacientes, (req, res) => {
-    if(req.usuprof) {
+router.get('/profissional_profile', authController.isLoggedIn, consultas.consultapacientes, (req, res) => {
+    if(req.usuario) {
         res.render('profissional_profile', {
-            //user: req.usuario
+            user: req.usuario
             pac: req.usuprof
         });
     } else {
