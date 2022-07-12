62f72e6b7470234101d6a032900bbaa3ceba8660
André Luiz Cunha de Oliveira <andreoliveira@ice.ufjf.br>
2022-07-12 09:49:39 -0300

    Update consultas.js

--- controllers/consultas.js
+++ controllers/consultas.js
@@ -55,7 +55,7 @@ exports.pacientes = async (req, res, next) => {
     try {
 
         db.query('SELECT * FROM lista as l inner join exercicios_lista as el on l.id_lista = el.id_lista join exercicios as e on el.id_exercicio = e.id_exercicio where l.id_paciente = 1 order by l.datahora_envio', (error, results) => {
-            console.log(results + "lista");
+            console.log(results);
 
             req.lista = results;
             return next();
