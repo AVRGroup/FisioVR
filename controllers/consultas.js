c89d771f82c8087c4c2a147027c0aeda357e3dcb
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-07-18 20:39:19 -0300

    Update consultas.js

--- controllers/consultas.js
+++ controllers/consultas.js
@@ -49,7 +49,7 @@ exports.consultapacientes = async (req, res, next) => {
 
     try {
         //const { testeusu } = req.body;
-        console.log(testandoooaqui);
+        console.log("testandoooaqui");
         console.log(req.body);
         db.query('SELECT * FROM paciente inner join usuario on paciente.id_usuario = usuario.id_usuario where id_prof_resp = 1', (error, results) => {
             //console.log(results);
