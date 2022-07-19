e1bd40a067d025eecb151c09ec0b7a36da339843
André Luiz Cunha de Oliveira <andreoliveira@ice.ufjf.br>
2022-07-19 10:26:54 -0300

    Update consultas.js

--- controllers/consultas.js
+++ controllers/consultas.js
@@ -95,7 +95,7 @@ exports.perfilPacientes = async (req, res, next) => {
         db.query('SELECT * FROM paciente inner join usuario on paciente.id_usuario = usuario.id_usuario where id_paciente = ?', [decoded.id], (error, results) => {
             console.log(results);
             console.log("results do perfilPacientes")
-            req.perfilPaciente = results[5];
+            req.perfilPaciente = results[decoded.id_usuario];
             return next();
         });
     } catch (error) {
