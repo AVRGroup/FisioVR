0df2a2d0ae0f4c31c7a63f09013be6a990d1ac39
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-07-26 09:23:05 -0300

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -30,7 +30,7 @@ router.get('/', testando.consultateste, (req, res) => {
 */
 
 router.get('/visualizarpaciente/:userpac', consultas.pacientes, (req, res) => {
-    const userpac = req.params.userpac;
+    //const userpac = req.params.userpac;
 
     res.render('visualizarpaciente', {
         infopac: req.lista
