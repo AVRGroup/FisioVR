90417a186c67f017641364da21b7f918aae56cb3
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-08-01 18:28:58 -0300

    Update consultas.js

--- controllers/consultas.js
+++ controllers/consultas.js
@@ -81,13 +81,13 @@ exports.pacientes = async (req, res, next) => {
             req.infolista = results2;
            // return next();
 
-            db.query('SELECT * FROM usuario join pacientes on pacientes.id_usuario = usuario.id_usuario where paciente.id_paciente = ?', [userpac], (error, results) => {
+            db.query('SELECT * FROM usuario join pacientes on pacientes.id_usuario = usuario.id_usuario where paciente.id_paciente = ?', [userpac], (error, results))
              console.log(results);
             // console.log("Lista")
              req.infopac = results;
             // return next();
              
-            });
+            
 
          return next();
         });
