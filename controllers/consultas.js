a3b834eb2b940ff22194bcc3b2926f3de0446459
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-07-31 23:15:52 -0300

    Update consultas.js

--- controllers/consultas.js
+++ controllers/consultas.js
@@ -78,13 +78,21 @@ exports.pacientes = async (req, res, next) => {
 
     try {
 
-        db.query('SELECT * FROM lista as l inner join exercicios_lista as el on l.id_lista = el.id_lista join exercicios as e on el.id_exercicio = e.id_exercicio where l.id_paciente = ? order by l.datahora_envio', [userpac], (error, results) => {
-            console.log(results);
-            console.log("Lista")
-            req.lista = results;
+        db.query('SELECT * FROM lista as l inner join exercicios_lista as el on l.id_lista = el.id_lista join exercicios as e on el.id_exercicio = e.id_exercicio where l.id_paciente = ? order by l.datahora_envio desc', [userpac], (error, results) => {
+           // console.log(results);
+           // console.log("Lista")
+            req.infolista = results;
             return next();
             
         });
+
+        db.query('SELECT * FROM usuario join pacientes on pacientes.id_usuario = usuario.id_usuario where paciente.id_paciente = ?', [userpac], (error, results) => {
+            // console.log(results);
+            // console.log("Lista")
+             req.infopac = results;
+             return next();
+             
+         });
 
       /*  db.query('SELECT * FROM paciente join usuario on paciente.id_usuario = usuario.id_usuario where paciente.id_paciente = ?', [userpac], (error, results2) => {
             console.log(results2);
