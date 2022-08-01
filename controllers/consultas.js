7ba82e3734660ffdb9bc95631601b679c340145e
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-07-31 23:34:46 -0300

    Update consultas.js

--- controllers/consultas.js
+++ controllers/consultas.js
@@ -48,18 +48,10 @@ exports.consultateste = async (req, res) => {
 
 exports.consultapacientes = async (req, res, next) => {
     //  console.log(req.cookies);
-       const userpac = req.params.userpac;
-
-
-
     try {
-        //const userpac = req.query.userpac;
-        //const { user, password } = req.body;
-        console.log("testandoooaqui");
-        console.log("consultando req body:" + req.params.login + "aaa" + req.params + "aaa" + userpac);
-        //console.log("consultando req query:" + req.query.0);
-        
-        db.query('SELECT * FROM paciente inner join usuario on paciente.id_usuario = usuario.id_usuario where id_prof_resp = 1', (error, results) => {
+        const decoded = await promisify(jwt.verify)(req.cookies.jwt,process.env.JWT_SECRET);
+          
+        db.query('SELECT * FROM paciente inner join usuario on paciente.id_usuario = usuario.id_usuario where id_prof_resp = ?', [decoded.id], (error, results) => {
             //console.log(results);
 
             req.usuprof = results;
