9cc5d3bb014c48176124cda69a462f5516759010
André Luiz Cunha de Oliveira <andreoliveira@ice.ufjf.br>
2022-08-30 13:51:44 -0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,5 @@ router.post('/login', authController.login);
 
 router.get('/logout', authController.logout);
 
-router.post('/atualizarDados', authController.atualizarDados);
 
 module.exports = router;
