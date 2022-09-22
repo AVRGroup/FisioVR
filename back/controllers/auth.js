ae804067d3c38e7e8c52170a18bc165fcf6ec85f
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-09-21 22:21:23 -0300

    Update auth.js

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -70,7 +70,7 @@ exports.login = async (req, res) => {
                     res.status(200).redirect("/cadastro");
                 } else if (tipo == 2) {
                     res.cookie('jwt', token, cookieOptions);
-                    res.status(200).redirect("/profissional_profile");
+                    res.status(200).redirect("/cadastro");
                 } else if (tipo == 3) {
                     res.cookie('jwt', token, cookieOptions);
                     res.status(200).redirect("/paciente");
