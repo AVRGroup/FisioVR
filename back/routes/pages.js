40a9d0d0caf0b856e31b087ac31c84db07f86bec
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-10-04 22:30:27 -0300

    Update pages.js

--- back/routes/pages.js
+++ back/routes/pages.js
@@ -28,7 +28,8 @@ router.get('/', authController.isLoggedIn, (req, res) => {
     });
 });
 
-router.get('/cadastro', consultas.tipos_usuarios, authController.register, (req, res) => {
+//authController.register,
+router.get('/cadastro', consultas.tipos_usuarios, (req, res) => {
     res.render('cadastro', {
         title: 'FisioVR - Cadastro',
         layout: 'main',
