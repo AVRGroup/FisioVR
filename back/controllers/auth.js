599011b03e133308885db8bf19bca9c6b0d6211c
dauanealmeida <dauanejoicee_jf@hotmail.com>
2022-10-05 23:11:30 -0300

    Update auth.js

--- back/controllers/auth.js
+++ back/controllers/auth.js
@@ -28,7 +28,7 @@ exports.login = async (req, res) => {
                 integrity: 'sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh',
                 crossorigin: 'anonymous'
             }],
-            navbar: [{  }],
+            navbar: [{ name: 'Cadastro', route: '/cadastro' }],
             user: req.usuario,
             message: ''
         }
