d4b836795b05a73dba114ae78d7e51fc28017034
Daniel <danielmrezende@yahoo.com.br>
2023-09-23 13:49:03 -0300

    Estilizando paginas

--- back/routes/pages.js
+++ back/routes/pages.js
@@ -90,7 +90,7 @@ router.get('/', authController.isLoggedIn, (req, res) => {
             integrity: 'sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh',
             crossorigin: 'anonymous'
         }],
-        navbar: [{name: 'Login', route: 'login'}],
+        navbar: [{name: 'Login', route: 'Login'}],
         user: req.usuario
     });
 });
@@ -572,7 +572,17 @@ router.get('/profissional', authController.isLoggedIn, (req, res) => {
 */
 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {
+        title: 'FisioVR - Login',
+        layout: 'main',
+        styleLibs: [{
+            href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css',
+            integrity: 'sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh',
+            crossorigin: 'anonymous'
+        }],
+        navbar: [{}],
+        user: req.usuario
+    });
 });
 
 
